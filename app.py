0de492f77619b67b816eb3d5cf03de27b6b0f362
srahuliitb <rahulsingh@ITRS868s-MacBook-Pro.local>
2020-03-03 22:21:11 +0530

    third commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///posts.db'
-db = SQLAlchemy(home_page)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
 
 class BlogPost(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -16,30 +16,45 @@ class BlogPost(db.Model):
 	def __repr__(self):
 		return 'Blog post' + str(self.id)
 
-
-all_posts = [
-    {
-        'title': 'Post 1',
-        'content': 'This is the first post.',
-        'author': 'Rahul Singh',
-        'date_posted': 'February 29, 2020'
-    },
-    {
-        'title': 'Post 2',
-        'content': 'This is the second post.',
-        'date_posted': 'March 3, 2020'
-    }
-]
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template('posts.html', posts=all_posts)
+	if request.method == 'POST':
+		post_title = request.form['title']
+		post_content = request.form['content']
+		post_author = request.form['author']
+		new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+		db.session.add(new_post)
+		db.session.commit()
+		return redirect('/posts')
+	else:
+		all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+		return render_template('posts.html', posts=all_posts)
+
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+	post = BlogPost.query.get_or_404(id)
+	db.session.delete(post)
+	db.session.commit()
+	return redirect('/posts')
+
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+	post = BlogPost.query.get_or_404(id)
+	if request.method == 'POST':
+		post.title = request.form['title']
+		post.author = request.form['author']
+		post.content = request.form['content']
+		db.session.commit()
+		return redirect('/posts')
+	else:
+		return render_template('edit.html', post=post)
 
 
 if __name__ == '__main__':
--- home_page.py
+++ home_page.py
@@ -1,16 +1,33 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =  'sqlite:///posts.db'
+db = SQLAlchemy(home_page)
+
+class BlogPost(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100), nullable=False) # This field is required. It can't be null.
+	content = db.Column(db.Text, nullable=False)
+	author = db.Column(db.String(20), nullable=False, default='N/A')
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+	def __repr__(self):
+		return 'Blog post' + str(self.id)
+
 
 all_posts = [
     {
-        'score': 'Computer score',
-        'content': 'Computer scored 50 points.',
-        'author': 'Rahul'
+        'title': 'Post 1',
+        'content': 'This is the first post.',
+        'author': 'Rahul Singh',
+        'date_posted': 'February 29, 2020'
     },
     {
-        'score': 'Player score',
-        'content': 'Player scored 100 points.'
+        'title': 'Post 2',
+        'content': 'This is the second post.',
+        'date_posted': 'March 3, 2020'
     }
 ]
 
@@ -20,7 +37,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
     return render_template('posts.html', posts=all_posts)
 
